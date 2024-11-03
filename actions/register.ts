c70819510149f882ad075aab3e0d4245ffefe248
omarques <64285626+OMARQUES@users.noreply.github.com>
2024-11-03 18:39:13 -0300

    Add funcionalidade de criar conta com email e senha

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,36 @@
 "use server"
 
-import { RegisterSchema } from "@/schemas"
 import * as z from "zod"
+import bcrypt from "bcrypt"
+
+import { RegisterSchema } from "@/schemas"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validadeFields = RegisterSchema.safeParse(values)
 
-    if(validadeFields.success) {
+    if(!validadeFields.success) {
         return {error: "Credenciais inválidas"}
     }
 
-    return {success: "Email enviado!"}
+    const { email, password, name } = validadeFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+    if(existingUser) {
+        return {error: "Email já está em uso!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    //Todo: Enviar email com token de verificação
+
+    return {success: "Usuario criado!"}
 }
