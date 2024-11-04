e3f3615bc64c33f0add0fa7946c86ec919cc2844
omarques <64285626+OMARQUES@users.noreply.github.com>
2024-11-04 15:30:41 -0300

    Session & JWT Callbacks

--- auth.ts
+++ auth.ts
@@ -1,13 +1,47 @@
-import NextAuth from "next-auth"
+import NextAuth, {type DefaultSession} from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, UserRole } from "@prisma/client"
 import authConfig from "./auth.config"
 
 import {db} from "@/lib/db"
- 
+import { getUserByID } from "./data/user"
+
+declare module "@auth/core" {
+  interface Session{
+    user:{
+      role: "ADMIN" | "USER"
+    }& DefaultSession["user"]
+  }
+}
+
 const prisma = new PrismaClient()
  
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const { auth, handlers, signIn, signOut 
+
+} = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      if(token.sub && session.user){
+        session.user.id = token.sub
+      }
+
+      if(token.role && session.user){
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+    async jwt({token}) {
+      if(!token.sub) return token
+
+      const existingUser = await getUserByID(token.sub)
+
+      if(!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
